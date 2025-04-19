4e644c9fb43763e1de784dd1cfaa52ca0a2d3460
KevinGJ23 <kevin.garciaj@udea.edu.co>
2025-04-19 18:54:07 -0500

    backup

--- Backend/System/functios.py
+++ Backend/System/functios.py
@@ -4,32 +4,32 @@ import pandas as pd
 import json as js
 
 # Función para cargar el archivo JSON como diccionario y enviarlo a MongoDB
+import json as js
+
 def cargar_json(ruta_json, patients=None):
     with open(ruta_json, 'r', encoding='utf-8') as archivo:
         pacientes = js.load(archivo)
+    
     if not pacientes:
-        return {}
-    data = pacientes[0]  # Tomar el primer paciente de la lista creada
+        return
+
+    data = pacientes[0]
     data_limpio = {key.strip(): value for key, value in data.items()}
 
-    # Verificamos y convertimos 'ID' a string si existe
     if 'id' in data_limpio:
         data_limpio['ID'] = str(data_limpio.pop('id'))
 
-    # Comprobamos si ya existe el ID en la base de datos
     if patients.find_one({'ID': data_limpio['ID']}):
-        return f"[ERROR] El documento con ID {data_limpio['ID']} ya existe en MongoDB."
-    
-    # Insertar en MongoDB si no existe
-    patients.insert_one(data_limpio)
-    phrase = f"[JSON] Documento insertado en MongoDB: {data_limpio.get('ID', '(sin ID)')}"
+        return
 
-    return phrase
+    patients.insert_one(data_limpio)
+
 
 # Función para cargar el archivo TXT como diccionario y enviarlo a MongoDB
-def cargar_txt(ruta_txt,patients=None):
+def cargar_txt(ruta_txt, patients=None):
     with open(ruta_txt, encoding='utf-8') as file:
         content = file.readlines()
+
     areas, tiempos = {}, {}
     nombre, apellido, paciente_id, fecha = "", "", "", ""
 
@@ -37,7 +37,7 @@ def cargar_txt(ruta_txt,patients=None):
         parts = line.strip().split('|')
 
         if '3O' in line:
-            nombre, apellido = parts[12].strip(), parts[13].strip() # Los datos del paciente cuando encuentra '3O'
+            nombre, apellido = parts[12].strip(), parts[13].strip()
             paciente_id = parts[2].strip()
 
         elif 'AREA' in line:
@@ -49,10 +49,9 @@ def cargar_txt(ruta_txt,patients=None):
             codigo = parts[2].split('^')[3].strip()
             tiempos[codigo] = parts[3].strip()
 
-    # Si se ha encontrado un paciente_id, crea el diccionario de datos
     if paciente_id:
-        fecha_formateada = f"{fecha[:4]}-{fecha[4:6]}-{fecha[6:8]}" # Formatea la fecha
-        
+        fecha_formateada = f"{fecha[:4]}-{fecha[4:6]}-{fecha[6:8]}"
+
         data_paciente3 = {
             "nombre": nombre,
             "apellido": apellido,
@@ -61,23 +60,21 @@ def cargar_txt(ruta_txt,patients=None):
             "MEDICIONES": areas,
             "TIEMPOS": tiempos
         }
-        
-        # Verifica si el ID ya existe en la base de datos
+
         if patients.find_one({"ID": paciente_id}):
-            return f"[ERROR] El documento con ID {paciente_id} ya existe en MongoDB."
-        
-        #SI no, se inserta el documento
+            return  # No insertar si ya existe
+
         patients.insert_one(data_paciente3)
-        
-        return f"[TXT] Documento insertado en MongoDB: {data_paciente3['ID']}"
-    else:
-        return "[ERROR] No se encontró un paciente con la información especificada."
+
 
 # Función para cargar el archivo CSV como diccionario y enviarlo a MongoDB
-def cargar_csv(ruta_csv,patients=None, delimitador=';'):
+import pandas as pd
+
+def cargar_csv(ruta_csv, patients=None, delimitador=';'):
     df = pd.read_csv(ruta_csv, delimiter=delimitador)
+    
     if df.empty:
-        return {}
+        return  # No hacer nada si el DataFrame está vacío
     
     # Tomar la primera fila y limpiar espacios
     data = df.to_dict(orient='records')[0]
@@ -89,12 +86,11 @@ def cargar_csv(ruta_csv,patients=None, delimitador=';'):
     
     # Comprobamos si ya existe el ID en la base de datos
     if patients.find_one({'ID': data_limpio['ID']}):
-        return f"[ERROR] El documento con ID {data_limpio['ID']} ya existe en MongoDB."
+        return  # No insertar si ya existe
     
     # Insertar en MongoDB si no existe
     patients.insert_one(data_limpio)
-    phrase = f"[CSV] Documento insertado en MongoDB: {data_limpio.get('ID', '(sin ID)')}"
-    return phrase
+
 
 def leer_archivos_dispositivos(nombre_archivo,database):
     base_dir = os.path.dirname(os.path.abspath(__file__))  # Ruta del script
@@ -102,12 +98,9 @@ def leer_archivos_dispositivos(nombre_archivo,database):
     for filename in os.listdir(ruta):
         file_path = os.path.join(ruta, filename)
         if file_path.endswith('.txt'):
-            respuesta= cargar_txt(file_path,database)
-            print(respuesta)
+            cargar_txt(file_path,database)
         elif file_path.endswith('.csv'):
-            respuesta= cargar_csv(file_path,database)
-            print(respuesta)
+            cargar_csv(file_path,database)
         elif file_path.endswith('.json'):
-            respuesta= cargar_json(file_path,database)
-            print(respuesta)
+            cargar_json(file_path,database)
 
